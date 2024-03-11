41fb8c2316509aa95d9bfc6a375e9ce56e850db3
Randall White <randall.white@login-02.clusternet>
2024-03-11 14:07:24 -0700

    last push before overhaul

---
+++ start_spid.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+## 03/07/24
+## start spid jupyter
+
+#note that the container already pretty much creates the directories, just make sure to bind `/hpc` and pass --nv to use gpus
+
+#container location
+CONTAINER_FULL_PATH='/hpc/apps/spid/master/bin/spid_container.sif'
+
+#noteboook location to launch everything 
+SPID_CACHE_DIR=${HOME}/.spid_cache
+if ! [ -d ${SPID_CACHE_DIR} ]; then
+    mkdir -p ${SPID_CACHE_DIR}
+fi
+
+NOTEBOOK_ROOT=${HOME}
+
+function create_random_port () 
+{
+
+    local MAX_PORT=65535
+    local SET_PORT_START=60000
+
+    PORT_DIFFERENCE=$((${MAX_PORT} - ${SET_PORT_START}))
+    VALS=($(seq 60000 65535))
+
+    echo ${VALS[$((($RANDOM + $PORT_DIFFERENCE) % 5535))]}
+
+}
+
+
+
+MY_PORT=$(create_random_port)
+
+# create server config
+export CONFIG_FILE="${PWD}/config.py"
+(
+umask 077
+cat > "${CONFIG_FILE}" << EOL
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = ${MY_PORT}
+c.NotebookApp.port_retries = 0
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.notebook_dir = "${NOTEBOOK_ROOT}"
+c.NotebookApp.disable_check_xsrf = True
+EOL
+)
+
+
+echo "Starting SPID Container w/ Jupyter Notebook"
+
+apptainer exec --writable-tmpfs --nv --bind /hpc:/hpc ${CONTAINER_FULL_PATH} jupyter notebook --config="${CONFIG_FILE}"
+
+exit 0 
+
+
